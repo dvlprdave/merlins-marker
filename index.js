5c1cf00861eeabe73e9f45e1d9a50854b9534c60
David <devdavee@gmail.com>
2020-01-10 21:31:28 -0500

    Set href for button

--- index.js
+++ index.js
@@ -2,20 +2,6 @@ import { colors } from './colorHelper'
 
 const inputs = [].slice.call(document.querySelectorAll('input[type="color"]'));
 
-async function get_css_and_replace_vars() {
-  const res = await fetch("./themes/prism.css");
-  const orig_css = await res.text();
-  let updated_css = orig_css;
-  const regexp = /(?:var\(--)[a-zA-z\-]*(?:\))/g;
-  let cssVars = orig_css.matchAll(regexp);
-  cssVars = Array.from(cssVars).flat();
-
-  for (const v of cssVars) {
-    updated_css = updated_css.replace(v, colors[v.slice(6, -1)]);
-  }
-  console.log(updated_css);
-}
-
 const handleThemeUpdate = (colors) => {
   const root = document.querySelector(':root');
   const keys = Object.keys(colors);
@@ -27,26 +13,47 @@ const handleThemeUpdate = (colors) => {
 inputs.forEach((input) => {
   input.addEventListener('change', (e) => {
     e.preventDefault()
-    const cssPropName = `--${e.target.id}`;
+    const cssPropName = `--${e.target.id}`
+
     document.styleSheets[2].cssRules[3].style.setProperty(cssPropName, e.target.value)
+
     handleThemeUpdate({
       [cssPropName]: e.target.value
     });
-    console.log(`${cssPropName} is now ${e.target.value}`)
-    console.log('stylesheet:', document.styleSheets[2].cssRules)
-    get_css_and_replace_vars()
+
+    updatedUserStyleSheet()
   });
 });
 
+const updatedUserStyleSheet = async () => {
+  const res = await fetch("./themes/prism.css");
+  const orig_css = await res.text();
+  let updated_css = orig_css;
 
-// const cssRules = document.styleSheets[2].cssRules;
-// for (var i = 0; i < cssRules.length; i++) {
-//   // Finds css variable names
-//   const regexp = /(?:var\(--)[a-zA-z\-]*(?:\))/
+  const regexp = /(?:var\(--)[a-zA-z\-]*(?:\))/g;
+  let cssVars = orig_css.matchAll(regexp);
+  cssVars = Array.from(cssVars).flat();
 
-//   let cssVariables = cssRules[i].cssText.matchAll(regexp)
-//   cssVariables = Array.from(cssVariables).join()
+  for await (const variable of cssVars) {
+    updated_css = updated_css.replace(variable, colors[variable.slice(6, -1)]);
+  }
 
-// }
+  // Download button
+  const downloadBtn = document.getElementById('download-btn')
+  downloadBtn.setAttribute('href', 'data:application/octet-stream;charset=utf-8,' + encodeURIComponent(updated_css))
 
+  console.log(updated_css)
+}
 
+updatedUserStyleSheet()
+
+// var a = window.document.createElement('a');
+// a.href = window.URL.createObjectURL(new Blob([updatedUserStyleSheet()], { type: 'text/css' }));
+// a.download = 'test.css';
+
+// // Append anchor to body.
+// document.body.appendChild(a);
+// a.click();
+
+// // Remove anchor from body
+// document.body.removeChild(a);
